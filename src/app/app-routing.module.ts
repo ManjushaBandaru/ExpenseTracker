e3fe3f3a3a7f109e7a060e2c493cd5af43b57baa
ManjushaBandaru <mailto:manjusha.b@calibrage.in>
2024-06-18 13:01:01 +0530

    changes sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  {path:'sidenav',component:SidenavComponent},
   { path: 'dashboard', loadChildren: () => import('./admin-dashboard/admin-dashboard.module').then(m => m.AdminDashboardModule) }];
 
 @NgModule({
