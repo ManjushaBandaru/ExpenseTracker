2eccbd9c53336c8d38d9f74e36614f7fc9bc951c
SaicharanDevi03 <saicharan.d@calibrage.in>
2024-06-18 18:02:08 +0530

    Changes done regarding registration form

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -7,15 +7,16 @@ import { Router } from '@angular/router';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
-  registrationform!:FormGroup;
-  constructor(private fb: FormBuilder, private route:Router) {}
+export class RegistrationComponent implements OnInit {
+  registrationform!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.registrationform = this.fb.group({
-      FirstName: ['', Validators.required],
-      LastName: ['', Validators.required],
-      MobileNumber: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      FirstName: ['', [Validators.required, Validators.pattern('^[A-Za-z]+$'), this.onlyAlphabets]],
+      LastName: ['', [Validators.required, Validators.pattern('^[A-Za-z]+$')]],
+      MobileNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
       Email: ['', [Validators.required, Validators.email]],
       UserName: ['', Validators.required],
       Password: ['', Validators.required]
@@ -24,21 +25,23 @@ export class RegistrationComponent {
 
   submit(): void {
     if (this.registrationform.valid) {
-      // Process form data here (e.g., send it to a server)
       console.log('Form Submitted', this.registrationform.value);
-      
-      // Navigate to the sidenav page
-      this.route.navigateByUrl('/sidenav');
+      this.router.navigateByUrl('/sidenav');
     } else {
-      // Mark all fields as touched to trigger validation messages
       this.registrationform.markAllAsTouched();
     }
   }
 
   closeForm(): void {
-    // Add any logic to close the form or navigate away
     console.log('Form closed');
-    this.route.navigateByUrl('/home'); // example route to navigate to home page
+    this.router.navigateByUrl('/home');
   }
-  
+
+  onlyAlphabets(event: KeyboardEvent): void {
+    const charCode = event.charCode || event.keyCode || event.which;
+    const inputChar = String.fromCharCode(charCode);
+    if (!/^[A-Za-z]+$/.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
 }
