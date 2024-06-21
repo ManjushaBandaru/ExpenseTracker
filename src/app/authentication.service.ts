44957291c35c7cad57e4b5ef3f6fafea14a0dff4
Azad-06 <azad.shaik@calibrage.in>
2024-06-21 18:05:41 +0530

    Authentication done in login page

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  url1 = 'https://localhost:44319/loginToken'
+
+  constructor(private Http: HttpClient) { }
+
+  tokenlogin(loginData: string, options: {headers:HttpHeaders}){
+    return this.Http.post(this.url1, loginData, options)
+  }
 }
